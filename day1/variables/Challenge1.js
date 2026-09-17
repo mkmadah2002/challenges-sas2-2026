b9220a038283f686c28d906eeaf5e8541b53663e
madah <mmadah352@gmail.com>
2026-09-17 10:51:26 +0100

    challang 1 ansewr

---
+++ day1/variables/Challenge1.js
@@ -0,0 +1,16 @@
+let pseudo = "zelda"
+let niveau = 12;
+let age = 21;
+let score = 4580;
+let pays = "maroc";
+
+
+console.log(`
+    Pseudo : ${pseudo}
+    Niveau : ${niveau}
+    Âge : ${age}
+    Score : ${score}
+    Pays : ${pays}`);
+
+
+
